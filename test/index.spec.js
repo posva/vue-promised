3703431e01cbc3d0f1e7263683b5cf2a376dfe29
Eduardo San Martin Morote <posva13@gmail.com>
2018-01-28 18:24:02 +0100

    ✅ Add test using faked-promise

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,13 +1,13 @@
 import { mount } from '@vue/test-utils'
+import fakePromise from 'faked-promise'
 import Helper from './utils/Helper'
 
-const tick = () => new Promise(resolve => setImmediate(resolve))
+const tick = () => new Promise(resolve => setTimeout(resolve, 0))
 
-describe('Tweezing', () => {
-  let wrapper
-  let promise
-  beforeEach(() => {
-    promise = Promise.resolve('foo')
+describe('Promised', () => {
+  let wrapper, promise, resolve, reject
+  beforeEach(async () => {
+    [promise, resolve, reject] = await fakePromise()
     wrapper = mount(Helper, {
       propsData: {
         promise,
@@ -16,7 +16,18 @@ describe('Tweezing', () => {
   })
 
   test('displays a loading screen waiting for the promise', async () => {
+    expect(wrapper.text()).toBe('loading')
+  })
+
+  test('displays the resolved value once resolved', async () => {
+    resolve('foo')
     await tick()
-    console.log(wrapper.text())
+    expect(wrapper.text()).toBe('foo')
+  })
+
+  test('displays an error if rejected', async () => {
+    reject(new Error('hello'))
+    await tick()
+    expect(wrapper.text()).toBe('hello')
   })
 })
