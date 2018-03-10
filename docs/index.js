0b5156cb4e67fb3c4c5da57dca1ebb542f72212a
Eduardo San Martin Morote <posva13@gmail.com>
2018-03-10 19:59:21 +0100

    📝 add code sample to examples

--- docs/index.js
+++ docs/index.js
@@ -1,4 +1,4 @@
-/* global Vue, VuePromised, axios */
+/* global Vue, VuePromised, axios, PrismComponent */
 // eslint-disable-next-line
 const delay = (t, value) => new Promise(r => setTimeout(r.bind(null, value), t))
 
@@ -11,6 +11,27 @@ function getRandomImage (max) {
     .then(res => res.data)
 }
 
+Vue.component('DemoCode', {
+  template: '#demo-code',
+  props: {
+    code: String,
+  },
+
+  data () {
+    return {
+      showCode: false,
+    }
+  },
+
+  computed: {
+    seeLabel () {
+      return this.showCode ? 'See demo' : 'See code'
+    },
+  },
+
+  components: { Prism: PrismComponent },
+})
+
 // eslint-disable-next-line
 new Vue({
   el: '#app',
@@ -18,6 +39,36 @@ new Vue({
   data: () => ({
     promise: null,
     promises: [],
+
+    samples: {
+      single: `\
+<Promised :promise="promise">
+  <div class="loading-spinner"></div>
+  <figure slot-scope="data">
+    <img :alt="data.transcript" :src="data.img"/>
+    <figcaption>#{{ data.num }} - {{ data.title }}</figcaption>
+  </figure>
+  <div slot="catch" slot-scope="error" class="message--error">
+    Error: {{ error.message }}
+  </div>
+</Promised>
+`,
+      multiple: `\
+<Promised :promises="promises">
+  <div class="loading-spinner"></div>
+  <div slot-scope="items">
+    <h3>{{ items.length }} promises resolved</h3>
+    <figure v-for="item in items">
+      <img :alt="item.transcript" :src="item.img"/>
+      <figcaption>#{{ item.num }} - {{ item.title }}</figcaption>
+    </figure>
+  </div>
+  <div slot="catch" slot-scope="errors" class="tip--error">
+    Failed {{ errors.length }} promises
+  </div>
+</Promised>
+`
+    },
   }),
 
   created () {
