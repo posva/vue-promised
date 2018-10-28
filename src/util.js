a413afa54d024b7da7b588f5f8e6558287eb6544
Eduardo San Martin Morote <posva13@gmail.com>
2018-10-28 11:28:14 +0100

    feat: remove assertions in production mode

--- src/util.js
+++ src/util.js
@@ -1,5 +1,5 @@
 export function assert (condition, message) {
-  if (!condition) {
+  if (process.env.NODE_ENV !== 'production' && !condition) {
     // useful when testing
     // console.warn(`[vue-promised] ${message}`)
     throw new Error(`[vue-promised] ${message}`)
