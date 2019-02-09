f1eb7b95969f2613e626de152ce05a8bd166cb1a
Eduardo San Martin Morote <posva13@gmail.com>
2019-02-09 16:04:21 +0100

    chore(deps): update

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,14 @@
 'use strict'
 
 module.exports = {
-  presets: ['@babel/preset-env'],
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: 'current',
+        },
+      },
+    ],
+  ],
 }
